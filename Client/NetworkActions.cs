6db1462f8fd74a4e4543aca743e9ef1867797108
davlveek <stankeev.vladimir@gmail.com>
2020-04-23 23:32:01 +0300

    Init and deinit methods

--- Client/NetworkActions.cs
+++ Client/NetworkActions.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net.Sockets;
+
 namespace Client
 {
-    class NetworkActions
+    class CryptoClient
     {
-        static public void SendData(byte[] data)
-        {
+        private readonly TcpClient Client;
+        private NetworkStream stream;
 
+        public CryptoClient(string address, int port) => Client.Connect(address, port);
+
+        ~CryptoClient() => Client.Close();
+
+        public void InitNetworkStream() => stream = Client.GetStream();
+
+        public void CloseNetworkStream() => stream.Close();
+
+        public void SendData(byte[] data)
+        {
+            
         }
     }
 }
